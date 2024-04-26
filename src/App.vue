b868c94be03f083e871343425f06450550e590be
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-04-26 14:20:17 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+console.log('hello !')
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">
+    Let's go to build Trello Board app 🐱‍🏍🐱‍🐉🐱‍🏍!
+  </h1>
+</template>
